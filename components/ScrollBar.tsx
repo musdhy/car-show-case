fd3f012a41b2c04daf76109090fbc130dafb38a3
call me mufasza <mmusdhaliph@gmail.com>
2023-06-24 14:24:39 +0100

    merge commit

--- components/ScrollBar.tsx
+++ components/ScrollBar.tsx
@@ -1,30 +1,44 @@
-'use client'
 import React, { useEffect, useState } from "react";
 import ProgressBar from "./ProgressBar";
 
-
-type Props = {}
+type Props = {};
 
 const Scrollbar = (props: Props) => {
-    const [scrollY, setScrollY] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        setScrollY(window.scrollY);
-      };
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    if (typeof window !== "undefined") {
       window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-  
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  let progressBarWidth = "0%";
+
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    progressBarWidth = `${Math.round(
+      (100 * scrollY) / document.body.scrollHeight
+    )}%`;
+  }
+
   return (
     <ProgressBar
       style={{
-        width: `${Math.round((100 * scrollY) / document.body.scrollHeight)}%`,
+        width: progressBarWidth,
         height: "3px",
         backgroundColor: "blue",
       }}
     />
   );
-}
+};
 
-export default Scrollbar
+export default Scrollbar;
